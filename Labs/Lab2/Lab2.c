f1dcad4118ab609586ca511b9e7b70e2c6c8c2d7
Magda_M <64345875+girl-in-tech@users.noreply.github.com>
2020-10-08 16:39:05 +0300

    Update Lab2.c

--- Labs/Lab2/Lab2.c
+++ Labs/Lab2/Lab2.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
-#include <math.h>
-  int main() { 
+# include <math.h>
+   int main() {
       int foo = 0;
       for (int n=1; n<=20; n++)
-      for (int i1=1; i1<n*n*n; i1+=2){
-          int i =i1;
-          while(foo != n*n*n && foo< n*n*n){
-              foo +=i;
-              i+=2;
-          }
-          if (foo == (n*n*n)){
-              foo = foo-i;
-              printf ("\n%d^3=%d", n,i);
-              i=i-2;
-              while (foo != 0 && foo>0){
-                  foo = foo-i;
-                  printf ("=%d",i);
-                  i=i-2;
+          for (int i1=1; i1<n*n*n; i1+=2){
+              int i =i1;
+              while(foo != n*n*n && foo< n*n*n){
+                  foo +=i;
+                  i=i+2;
               }
-           }
+          i -=2;
+          if (foo == (n*n*n)){
+            printf("%d^3=%d",n,i);
+            foo-=i;
+            while(foo != 0){
+                i-=2;
+                foo-=i;
+                printf("+%d",i);
+            }
+            printf("\n");
+          }
           foo=0;
       }
       return 0;
-          }
+}
